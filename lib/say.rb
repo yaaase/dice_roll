a5665665b7daacf918c978931cd4d9e2a718eb4c
Mark Billie <mbillie1@gmail.com>
2014-04-06 14:43:01 -0600

    module and method for random stuff to say

---
+++ lib/say.rb
@@ -0,0 +1,15 @@
+module Say
+
+  def say_take_a_shot
+    system("say -v Victoria 'take a fucking shot'")
+  end
+
+  def say_snake_eyes
+    system("say -v Alex 'snake eyes'")
+  end
+
+  def say_double_sixes
+    system("say -v Vicki 'double sixes'")
+  end
+
+end
